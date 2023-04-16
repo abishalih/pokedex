359e77bcf83f06d7a776b40320fc13b8b0df4725
Habibullah Shalihuddin <habibullahshalihuddin@outlook.com>
2023-04-17 00:10:58 +0700

    feature: create service to get list pokemon and detail pokemon

---
+++ src/infrastructure/service/pokedex.ts
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { API_POKEMON } from "../../application/common/api";
+import { PokemonProps } from "../../presentation/containers/PokemonThumbnail";
+
+export async function getListPokemon(setLoading:(isLoading:boolean)=>void) {
+    try {
+        setLoading(true)
+        const { data } = await axios.get(`${API_POKEMON}?limit=100000&offset=0`);
+        const res: Array<PokemonProps> = data.results ? data.results : data || [];
+        const result: Array<PokemonProps> = await Promise.all(res.map(({ url = "" }) => getDetailPokemon(url))).finally(()=>setLoading(false));
+        return result;
+    } catch (error) {
+        console.error("Failed to fetch options", error);
+        return [];
+    }
+};
+
+type TypesProps = {
+    slot?: number;
+    type?: {
+      name?: string;
+      url?: string;
+    };
+  };
+export type DetailProps = {
+    complete?: boolean;
+    id?: number;
+    name?: string;
+    url?: string;
+    types?: Array<TypesProps>;
+  };
+  
+export async function getDetailPokemon(url:string=  ""){
+    try {
+        const { data } = await axios.get(url);
+        const result:DetailProps = await data.results?data.results:data;
+        return {...result, types: result.types?.map(({ type })=> type?.name)}
+    } catch (error) {
+        console.error("Failed to fetch options", error);
+        return {};
+    }
+};
